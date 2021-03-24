cf485fc58c44de0dfd1fb51a6a91c40f06628983
Artem Shkuratov <shuratov2001@mail.ru>
2021-03-24 21:55:44 +0800

    created the skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+import Input from "./components/Input"
+import Output from "./components/Output"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div>
+      <Header title="Weather today" description="What the weather like today?"/>
+      <Input/>
+      <Output/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
