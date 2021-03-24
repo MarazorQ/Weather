229f42a26a983665fe4bdc3f1c93a737d1bba506
Artem Shkuratov <shuratov2001@mail.ru>
2021-03-24 22:27:39 +0800

    added some logic for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from "./components/Header"
 import Input from "./components/Input"
 import Output from "./components/Output"
+import {useState} from "react"
+
+const API_KEY = "560fa248c2896982c86a30538e05c590"
 
 const App = () =>{
+  const [stateApp, setApp] = useState({
+    value: ""
+  })
+  console.log(` APP: ${stateApp.value}`)
+  const getWeatherToday = async (e) =>{
+      e.preventDefault()
+      console.log("hello fro app")
+  }
   return(
     <div>
       <Header title="Weather today" description="What the weather like today?"/>
-      <Input/>
+      <Input getWeatherToday={getWeatherToday} setApp={setApp} stateApp={stateApp}/>
       <Output/>
     </div>
   )
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,23 @@
-const Input = () =>{
+import {useState} from "react"
+const Input = (props) =>{
+    const [stateValue, setValue] = useState({
+        value: ""
+    })
+    const HandaleChange = (e) =>{
+        let text = e.target.value
+        setValue({...stateValue,
+            value: text
+        })
+        props.setApp({...props.stateApp,
+            value: text
+        })
+        console.log(text)
+    }
+
     return(
-        <form>
+        <form onSubmit={props.getWeatherToday}>
             <div>
-                <input type="text"/>
+                <input type="text" value={stateValue.value} onChange={HandaleChange}/>
             </div>
             <div>
                 <button>Discover</button>
